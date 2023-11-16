ed9602f2985e9448e55bbe6d69cba4c7d92fa82f
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-16 16:08:49 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import { BlurView } from 'expo-blur';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import { Dimensions, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Dimensions, ImageBackground, StyleSheet, Text, View, FlatList } from 'react-native';
 import Services from './services';
-const { width, height } = Dimensions.get('screen')
 export default function App() {
+  const [userData, setUserData] = useState(null);
+  const { width, height } = Dimensions.get('screen')
+
 
   useEffect(() => {
-    Services.getUser
+    const fetchData = async () => {
+      try {
+        const data = await Services.getUser();
+        setUserData(data);
+      } catch (error) {
+        console.error('Error fetching data in Home:', error);
+      }
+    };
+
+    fetchData();
   }, [])
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <ImageBackground style={{ width: width, height: height }} source={require('./assets/pi.jpg')}>
         <BlurView tint='dark' intensity={100} style={{ width: width, height: height }} >
+
+          <FlatList data={userData} numColumns={4}
+            renderItem={({ item }) =>
+              <Text style={{ color: 'white', fontWeight: 'bold', marginTop: 50, padding: 10 }} key={item.id}>Codigo:{item.id}</Text>
+            }
+          />
 
         </BlurView>
       </ImageBackground>
@@ -24,7 +41,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
