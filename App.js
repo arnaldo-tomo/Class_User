21c075f37a8076697bf897bd4379b9a0e9477a98
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-20 22:48:28 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { BlurView } from 'expo-blur';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { Dimensions, ImageBackground, StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { Dimensions, ImageBackground, StyleSheet, Text, View, FlatList, TouchableOpacity, SafeAreaView } from 'react-native';
 import Services from './services';
+import { LinearGradient } from 'expo-linear-gradient';
 export default function App() {
   const [userData, setUserData] = useState(null);
   const { width, height } = Dimensions.get('screen')
@@ -23,23 +24,36 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <StatusBar style="light" />
-      <ImageBackground style={{ width: width, height: height }} source={require('./assets/pi.jpg')}>
-        <BlurView tint='dark' intensity={100} style={{ width: width, height: height }} >
+      <SafeAreaView style={{ width: width, height: height }}>
 
+        <StatusBar style="light" />
+        <ImageBackground style={{ width: width, height: height }} source={require('./assets/home.jpg')} >
+          {/* <BlurView tint='dark' intensity={100} style={{ width: width, height: height }} > */}
+          <LinearGradient
+            style={{
+              height: height,
+              width: width,
+            }}
+
+            colors={['rgba(0,0,0,80)', 'transparent']}
+            start={{ x: 0.1, y: 0.9 }}
+            end={{ y: 0.1, x: 0.2 }}
+          >
+
+          </LinearGradient>
           {userData == null ?
             <Text style={{ color: 'white', fontWeight: 'bold', marginTop: 50, padding: 10, justifyContent: 'center', alignItems: 'center' }}>Carregando....</Text> :
             <FlatList data={userData} numColumns={1}
               renderItem={({ item }) =>
                 <TouchableOpacity onPress={() => Services.set(item, item.id)}>
-                  <Text style={{ color: 'white', fontWeight: 'bold', marginTop: 50, padding: 10 }} key={item.id} >Codigo:{item.id}</Text>
+                  <Text style={{ color: 'white', fontWeight: 'bold', padding: 10, marginTop: 20 }} key={item.id} >Codigo:{item.id}</Text>
                 </TouchableOpacity>
               }
             />
           }
-
-        </BlurView>
-      </ImageBackground>
+          {/* </BlurView> */}
+        </ImageBackground>
+      </SafeAreaView>
     </View>
   );
 }
