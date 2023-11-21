1b0cd2c7d54b4fbf4310f28c54a26579e5afb036
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-22 00:19:30 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { BlurView } from 'expo-blur';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { Dimensions, ImageBackground, StyleSheet, Text, View, FlatList, TouchableOpacity, SafeAreaView } from 'react-native';
+import { Dimensions, ImageBackground, StyleSheet, Text, View, FlatList, TouchableOpacity, SafeAreaView, Image } from 'react-native';
 import Services from './services';
 import { LinearGradient } from 'expo-linear-gradient';
+import { ScrollView } from 'react-native';
 export default function App() {
   const [userData, setUserData] = useState(null);
   const { width, height } = Dimensions.get('screen')
@@ -13,7 +14,7 @@ export default function App() {
     const fetchData = async () => {
       try {
         const data = await Services.getUser();
-        setUserData(data);
+        setUserData(data.products);
       } catch (error) {
         console.error('Error fetching data in Home:', error);
       }
@@ -27,31 +28,45 @@ export default function App() {
       <SafeAreaView style={{ width: width, height: height }}>
 
         <StatusBar style="light" />
-        <ImageBackground style={{ width: width, height: height }} source={require('./assets/home.jpg')} >
-          <BlurView tint='dark' intensity={100} style={{ width: width, height: height }} >
-          <LinearGradient
-            style={{
-              height: height,
-              width: width,
-            }}
-
-            colors={['rgba(0,0,0,80)', 'transparent']}
-            start={{ x: 0.1, y: 0.9 }}
-            end={{ y: 0.1, x: 0.2 }}
-          >
-
-          </LinearGradient>
+        <ImageBackground style={{ width: width, height: height }} source={require('./assets/pi.jpg')} >
+          {/* <BlurView tint='light' intensity={1} style={{ width: width, height: height }} > */}
           {userData == null ?
             <Text style={{ color: 'white', fontWeight: 'bold', marginTop: 50, padding: 10, justifyContent: 'center', alignItems: 'center' }}>Carregando....</Text> :
+
             <FlatList data={userData} numColumns={1}
               renderItem={({ item }) =>
-                <TouchableOpacity onPress={() => Services.set(item, item.id)}>
-                  <Text style={{ color: 'white', fontWeight: 'bold', padding: 10, marginTop: 20 }} key={item.id} >Codigo:{item.id}</Text>
+                <TouchableOpacity onPress={() => Services.set(item, item.id)} key={item.id} style={{ padding: 20, paddingTop: 20, paddingBottom: 20, }}>
+                  {!item.thumbnail ?
+                    <Text>Carregando Imagen</Text>
+                    :
+                    <Image source={{ uri: item.thumbnail }} style={{ width: 200, height: 200, overflow: 'visible', resizeMode: 'contain' }}></Image>
+                  }
+                  <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20, }} >{item.title}</Text>
+                  <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15, }} >{item.description}</Text>
+                  <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15, }} >{item.price}</Text>
+                  <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15, }} >{item.discountPercentage}</Text>
+                  <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15, }} >{item.rating}</Text>
+                  <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15, }} >{item.stock}</Text>
+                  <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15, }} >{item.brand}</Text>
+                  <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 15, }} >{item.category}</Text>
                 </TouchableOpacity>
+
               }
             />
           }
-          </BlurView>
+          {/* <LinearGradient
+              style={{
+                height: height,
+                width: width,
+              }}
+
+              colors={['rgba(0,0,0,80)', 'transparent']}
+              start={{ x: 0.1, y: 1 }}
+              end={{ x: 0.1, y: 0.8 }}
+            >
+
+            </LinearGradient> */}
+          {/* </BlurView> */}
         </ImageBackground>
       </SafeAreaView>
     </View>
